553c4c9c33d191d04b205ef0229b3fad4c29fc22
Michael Robinson <michael.robinson@solnet.co.nz>
2016-05-13 15:32:19 +1200

    Copied YCM detailed completion lookups

--- rplugin/python3/deoplete/sources/typescript.py
+++ rplugin/python3/deoplete/sources/typescript.py
@@ -1,11 +1,12 @@
+import os
 import re
 import json
 import subprocess
 
-from logging import getLogger
+from tempfile import NamedTemporaryFile
 from deoplete.sources.base import Base
 
-logger = getLogger(__name__)
+MAX_COMPLETION_DETAIL = 100
 
 _tsserver_handle = subprocess.Popen("tsserver",
         stdout = subprocess.PIPE,
@@ -23,7 +24,6 @@ class Source(Base):
         self.mark = "[ts]"
         self.filetypes = ["typescript"]
         self.input_pattern = "\."
-        self.is_bytepos = True
 
         self._sequenceid = 0
         self._current_file = None
@@ -39,7 +39,7 @@ class Source(Base):
         if arguments:
             request[ "arguments" ] = arguments
 
-        logger.debug("_send_request: request: {0}".format(request))
+        self.debug("_send_request: request: {0}".format(request))
 
         self._write_message(request)
 
@@ -54,7 +54,6 @@ class Source(Base):
             if len(headerline):
                 key, value = headerline.split( ":", 2 )
                 headers[ key.strip() ] = value.strip()
-                logger.debug(headers)
                 break
 
         if "Content-Length" not in headers:
@@ -74,39 +73,91 @@ class Source(Base):
     def relative_file(self):
         return self.vim.eval("expand('%:p')")
 
+    def on_buffer(self, context):
+        self._send_request("open", {
+            "file": self.relative_file()
+        });
+
+    def _reload(self):
+        contents = self.vim.eval("join(getline(1,'$'), \"\n\")")
+        self.debug(contents)
+        tmpfile = NamedTemporaryFile(delete = False)
+        tmpfile.write(contents.encode('utf-8'))
+        tmpfile.close()
+        self._send_request('reload', {
+            'file': self.relative_file(),
+            'tmpfile': tmpfile.name
+        })
+        # os.unlink(tmpfile.name)
+
     def get_complete_position(self, context):
         m = re.search(r"\w*$", context["input"])
         return m.start() if m else -1
 
     def gather_candidates(self, context):
-        self.debug("gather_candidates: contenxt: {0}".format(context))
+        self.debug("\n")
 
-        self._send_request("open", {
-            "file": self.relative_file()
-        });
+        self._reload()
 
         line = context["position"][1]
         col = context["complete_position"] + 1
 
-        completionsRequestBody = {
+        data = self._send_request("completions", {
             "file":   self.relative_file(),
             "line":   line,
             "offset": col,
             "prefix": context["complete_str"]
+        }, wait_for_response = True)
+
+        # exit early if no data
+        self.debug(data)
+        if data is None or not "body" in data:
+            return []
+
+        # if there are too many entries, just return basic completion
+        if len(data["body"]) > MAX_COMPLETION_DETAIL:
+            return [self._convert_completion_data(e) for e in data["body"]]
+
+        # build up more helpful completion data
+        names = []
+        maxNameLength = 0
+        for entry in data["body"]:
+            names.append(entry["name"])
+            maxNameLength = max(maxNameLength, len(entry["name"]))
+
+        detailed_data = self._send_request('completionEntryDetails', {
+            "file":   self.relative_file(),
+            "line":   line,
+            "offset": col,
+            "entryNames": names
+        }, wait_for_response = True)
+
+        self.debug(detailed_data)
+        if detailed_data is None or not "body" in detailed_data:
+            return []
+
+        return [self._convert_detailed_completion_data(e, padding = maxNameLength)
+             for e in detailed_data["body"]]
+
+    def _convert_completion_data(self, entry):
+        return {
+            "word": entry["name"],
+            "kind": entry["kind"],
+            "menu": entry["kindModifiers"]
         }
 
-        self.debug("gather_candidates: completions request: {0}".format(completionsRequestBody))
-        data = self._send_request("completions", completionsRequestBody, wait_for_response = True)
+    def _convert_detailed_completion_data(self, entry, padding = 80):
+        self.debug(entry)
 
-        completions = []
-        if data is not None and "body" in data:
-            for rec in data["body"]:
-                completions.append({
-                    "word": rec["name"],
-                    "kind": rec["kind"],
-                    "menu": rec["kindModifiers"]
-                    })
+        name = entry["name"]
+        display_parts = entry['displayParts']
+        signature = ''.join([p['text'] for p in display_parts])
 
-        logger.debug("gather_candidates: returning: {0}".format(completions))
-
-        return completions
+        # needed to strip new lines and indentation from the signature
+        signature = re.sub( '\s+', ' ', signature )
+        menu_text = '{0} {1}'.format(name.ljust(padding), signature)
+        return ({
+            "word": name,
+            "kind": entry["kind"],
+            "menu": menu_text
+        })
