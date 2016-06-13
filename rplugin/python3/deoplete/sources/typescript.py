bc3901543bfb86559dd18fb4d90ad52a4dd13b11
mhartington <mikehartington@gmail.com>
2016-06-12 21:11:00 -0400

    finally working!

--- rplugin/python3/deoplete/sources/typescript.py
+++ rplugin/python3/deoplete/sources/typescript.py
@@ -3,10 +3,17 @@ import re
 import json
 import subprocess
 import platform
+import itertools
+
+from threading import Thread
+from threading import Event
+from threading import Lock
+
 from tempfile import NamedTemporaryFile
 from deoplete.sources.base import Base
 
 MAX_COMPLETION_DETAIL = 50
+RESPONSE_TIMEOUT_SECONDS = 20
 
 
 class Source(Base):
@@ -22,58 +29,26 @@ class Source(Base):
         self.mark = "TS"
         self.rank = 700
         self.input_pattern = r'\.\w*'
+        # self.input_pattern = '.'
 
         # Project related
-        self._project_directory = None
+        self._project_directory = os.getcwd()
         self._sequenceid = 0
-        self._current_file = None
-        self._tsserver_handle = None
-
-    # Start the server process
-    def start_server(self):
-        self.debug("Starting TSSERVER")
-        self.search_tss_project_dir()
-
-        self.debug(self._project_directory)
-        env = None
-        if platform.system() == 'Darwin':
-            env = os.environ.copy()
-            env['PATH'] += ':/usr/local/bin'
+        self._environ = os.environ.copy()
         self._tsserver_handle = subprocess.Popen("tsserver",
-                cwd = self._project_directory,
-                env = env,
-                stdout = subprocess.PIPE,
-                stdin = subprocess.PIPE,
-                stderr = subprocess.STDOUT,
-                universal_newlines = True,
-                bufsize = 1)
-        self.on_event(None)
+                                                 env=self._environ,
+                                                 cwd=self._project_directory,
+                                                 stdout=subprocess.PIPE,
+                                                 stdin=subprocess.PIPE,
+                                                 stderr=subprocess.STDOUT,
+                                                 universal_newlines=True,
+                                                 shell=True,
+                                                 bufsize=1)
 
-    # Get the cwd
-    def search_tss_project_dir(self):
-        if not self._project_directory:
-            directory = self.vim.eval("expand('%:p:h')")
+    def search_tss_project_dir(self, context):
+        self._project_directory = context['cwd']
 
-            if not os.path.isdir(directory):
-                return ''
-
-            if directory:
-                self._project_directory = directory
-                while True:
-                    parent = os.path.dirname(directory[:-1])
-
-                    if not parent:
-                        self._project_directory = self.vim.eval('getcwd()')
-                        break
-
-                    if os.path.isfile(os.path.join(directory, 'tsconfig.json')):
-                        self._project_directory = directory
-                        break
-                    directory = parent
-
-    # builds up the request and calls _write_message
-    def _send_request( self, command, arguments = None, wait_for_response = False ):
-
+    def _sendReuest(self, command, arguments=None, wait_for_response=False):
         seq = self._next_sequence_id()
         request = {
             "seq":     seq,
@@ -81,9 +56,8 @@ class Source(Base):
             "command": command
         }
         if arguments:
-            request[ "arguments" ] = arguments
+            request["arguments"] = arguments
 
-        # self.debug("_send_request: request: {0}".format(request))
         self._write_message(request)
 
         if not wait_for_response:
@@ -93,104 +67,89 @@ class Source(Base):
         headers = {}
         while True:
             headerline = self._tsserver_handle.stdout.readline().strip()
-            linecount += 1;
+            linecount += 1
             if len(headerline):
-                key, value = headerline.split( ":", 2 )
-                headers[ key.strip() ] = value.strip()
+                key, value = headerline.split(":", 2)
+                headers[key.strip()] = value.strip()
                 break
-
         if "Content-Length" not in headers:
-            raise RuntimeError( "Missing 'Content-Length' header" )
+            raise RuntimeError("Missing 'Content-Length' header")
         contentlength = int(headers["Content-Length"])
         return json.loads(self._tsserver_handle.stdout.read(contentlength))
-
-    # increment sequence ids
-    def _next_sequence_id(self):
-        seq = self._sequenceid
-        self._sequenceid += 1
-        return seq
 
     # take the command build-up, and post it subprocess
     def _write_message(self, message):
         self._tsserver_handle.stdin.write(json.dumps(message))
         self._tsserver_handle.stdin.write("\n")
 
-    #  Get the path to the project to set context.
-    #  Needed to make tsconfig work.
+    def _reload(self):
+        filename = self.relative_file()
+        contents = self.vim.eval("join(getline(1,'$'), \"\n\")")
+        tmpfile = NamedTemporaryFile(delete=False)
+        tmpfile.write(contents.encode('utf-8'))
+        tmpfile.close()
+        self._sendReuest('reload', {
+            'file': filename,
+            'tmpfile': tmpfile.name
+        }, wait_for_response=True)
+        # os.unlink(tmpfile.name)
+
     def relative_file(self):
         return self.vim.eval("expand('%:p')")
 
-    def on_event(self, context):
-        self.debug("on_event: {0}".format(context))
-        self._send_request("open", {
-            "file": self.relative_file()
-        });
+    def _next_sequence_id(self):
+        seq = self._sequenceid
+        self._sequenceid += 1
+        return seq
 
-    # Send a reload command to make tsserver update with context
-    def _reload(self):
-        contents = self.vim.eval("join(getline(1,'$'), \"\n\")")
-        tmpfile = NamedTemporaryFile(delete = False)
-        tmpfile.write(contents.encode('utf-8'))
-        tmpfile.close()
-        self._send_request('reload', {
-            'file': self.relative_file(),
-            'tmpfile': tmpfile.name
-        })
-        # os.unlink(tmpfile.name)
+    def on_event(self, context):
+        # if context['event'] == 'BufEnter':
+        self.debug('BUFENTER')
+        self._sendReuest('open', {'file': self.relative_file()})
 
     # Get cursor position in the buffer
     def get_complete_position(self, context):
         m = re.search(r"\w*$", context["input"])
         return m.start() if m else -1
 
-    # Gets completion, calls self._send_request
     def gather_candidates(self, context):
-        # If the server is running, start it
-        if self._tsserver_handle is None:
-            self.start_server()
-
-        # TODO: Calling reload here messes with completions
-        # we end up firing reload multiple times and the context
-        # keeps getting shifted.
-        # self._reload()
-        line = context["position"][1]
-        col = context["complete_position"] + 1
-
-        data = self._send_request("completions", {
+        self._reload()
+        data = self._sendReuest("completions", {
             "file":   self.relative_file(),
-            "line":   line,
-            "offset": col,
+            "line":   context["position"][1],
+            "offset": context["complete_position"] + 1,
             "prefix": context["complete_str"]
-        }, wait_for_response = True)
+        }, wait_for_response=True)
 
-        # exit early if no data
+        self.debug('*' * 25)
+        self.debug(data)
+        self.debug('*' * 25)
+
         if data is None or not "body" in data:
             return []
-
         # if there are too many entries, just return basic completion
-        if len(data["body"]) > MAX_COMPLETION_DETAIL:
-            return [self._convert_completion_data(e) for e in data["body"]]
+        # if len(data["body"]) > MAX_COMPLETION_DETAIL:
+        return [self._convert_completion_data(e) for e in data["body"]]
 
         # build up more helpful completion data
-        names = []
-        maxNameLength = 0
-        for entry in data["body"]:
-            names.append(entry["name"])
-            maxNameLength = max(maxNameLength, len(entry["name"]))
+        # names = []
+        # maxNameLength = 0
+        # for entry in data["body"]:
+        #     names.append(entry["name"])
+        #     maxNameLength = max(maxNameLength, len(entry["name"]))
+        #
+        # detailed_data = self._sendReuest('completionEntryDetails', {
+        #     "file":   self.relative_file(),
+        #     "line":   context["position"][1],
+        #     "offset": context["complete_position"],
+        #     "entryNames": names
+        # }, wait_for_response=True)
 
-        detailed_data = self._send_request('completionEntryDetails', {
-            "file":   self.relative_file(),
-            "line":   line,
-            "offset": col,
-            "entryNames": names
-        }, wait_for_response = True)
+        # if detailed_data is None or not "body" in detailed_data:
+        # return []
 
-        if detailed_data is None or not "body" in detailed_data:
-            return []
-
-        return [self._convert_detailed_completion_data(e, padding = maxNameLength)
-             for e in detailed_data["body"]]
-
+        # return [self._convert_detailed_completion_data(e, padding=maxNameLength)
+        # for e in detailed_data["body"]]
 
     # If the results are over 100, return a simplified list
     def _convert_completion_data(self, entry):
@@ -203,19 +162,18 @@ class Source(Base):
 
     # Under 100, provide more detail
     # TODO: Send method signature to preview window
-    def _convert_detailed_completion_data(self, entry, padding = 80):
+    def _convert_detailed_completion_data(self, entry, padding=80):
         # self.debug(entry)
 
         name = entry["name"]
-        display_parts = entry['displayParts']
-        signature = ''.join([p['text'] for p in display_parts])
+        # display_parts = entry['displayParts']
+        # signature = ''.join([p['text'] for p in display_parts])
 
         # needed to strip new lines and indentation from the signature
-        signature = re.sub( '\s+', ' ', signature )
-        menu_text = '{0} {1}'.format(name.ljust(padding), signature)
-        self.debug(signature)
+        # signature = re.sub( '\s+', ' ', signature )
+        # menu_text = '{0} {1}'.format(name.ljust(padding), signature)
         return ({
             "word": name,
             "kind": entry["kind"],
-            "info": menu_text
+            # "info": menu_text
         })
