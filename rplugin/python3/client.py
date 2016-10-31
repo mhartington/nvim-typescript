544c669616b9dbcce26706804e96d6816a498458
TiuSh <masy.mathieu@gmail.com>
2016-10-31 10:49:20 +0100

    feat(): extract server client to its own module Closes #27

---
+++ rplugin/python3/client.py
@@ -0,0 +1,198 @@
+import os
+import json
+import subprocess
+
+class Client:
+    __server_handle = None
+    __server_seq = 0
+    __project_directory = os.getcwd()
+    __environ = os.environ.copy()
+
+    def __init__(self, log_fn=None, debug_fn=None):
+        self.log_fn = log_fn
+        self.debug_fn = debug_fn
+
+    @classmethod
+    def __get_next_seq(cls):
+        seq = cls.__server_seq
+        cls.__server_seq += 1
+        return seq
+
+    def __log(self, message):
+        if self.log_fn:
+            self.log_fn(message)
+
+    def __debug(self, message):
+        if self.debug_fn:
+            self.debug_fn(message)
+
+    def __start_server(self):
+        if Client.__server_handle:
+            return
+
+        Client.__server_handle = subprocess.Popen(
+            "tsserver",
+            env=Client.__environ,
+            cwd=Client.__project_directory,
+            stdout=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            universal_newlines=True,
+            shell=True,
+            bufsize=1
+        )
+
+        self.__log("TSServer started")
+
+    def __send_data_to_server(self, data):
+        Client.__server_handle.stdin.write(json.dumps(data))
+        Client.__server_handle.stdin.write("\n")
+
+    def __get_response_body(self, response, default=[]):
+        success = bool(response) and "success" in response and response["success"]
+
+        # Should we raise an error if success == False ?
+
+        return response["body"] if success and "body" in response else default
+
+    def send_request(self, command, arguments=None, wait_for_response=False):
+        """
+            Sends a properly formated request to the server
+
+            :type command: string
+            :type arguments: dict
+            :type wait_for_response: boolean
+        """
+        # Make sure server is started
+        self.__start_server()
+
+        # Load next seq id
+        seq = Client.__get_next_seq()
+
+        # Build request
+        request = {
+            "seq": seq,
+            "type": "request",
+            "command": command
+        }
+
+        # Add arguments if provided
+        if arguments:
+            request["arguments"] = arguments
+
+        self.__debug("Request: {}".format(json.dumps(request)))
+
+        # Send request
+        self.__send_data_to_server(request)
+
+        if not wait_for_response:
+            return
+
+        linecount = 0
+        headers = {}
+
+        while True:
+            headerline = Client.__server_handle.stdout.readline().strip()
+            linecount += 1
+
+            if len(headerline):
+                key, value = headerline.split(":", 2)
+                headers[key.strip()] = value.strip()
+
+                # Response should contain a "Content-Length" header
+                if "Content-Length" not in headers:
+                    raise RuntimeError("Missing 'Content-Length' header")
+
+                # Read response content
+                contentlength = int(headers["Content-Length"])
+                ret_str = Client.__server_handle.stdout.read(contentlength)
+
+                self.__debug("Response: {}".format(ret_str))
+
+                ret = json.loads(ret_str)
+
+                # Each response should contain a "request_seq"
+                if "request_seq" not in ret or ret["request_seq"] > seq:
+                    return None
+                elif ret["request_seq"] == seq:
+                    return ret
+
+    def open(self, file):
+        """
+            Sends an "open" request
+
+            :type file: string
+        """
+        args = {"file": file}
+        self.send_request("open", args)
+
+    def close(self, file):
+        """
+            Sends a "close" request
+
+            :type file: string
+        """
+        args = {"file": file}
+        self.send_request("close", args)
+
+    def saveto(self, file, tmpfile):
+        """
+            Sends a "saveto" request
+
+            :type file: string
+            :type tmpfile: string
+        """
+        args = {"file": file, "tmpfile": tmpfile}
+        self.send_request("saveto", args)
+
+    def reload(self, file, tmpfile):
+        """
+            Sends a "reload" request
+
+            :type file: string
+            :type tmpfile: string
+        """
+        args = {"file": file, "tmpfile": tmpfile}
+        response = self.send_request("reload", args, True)
+
+        return response["success"] if response and "success" in response else False
+
+    def completions(self, file, line, offset, prefix=""):
+        """
+            Sends a "completions" request
+
+            :type file: string
+            :type line: int
+            :type offset: int
+            :type prefix: string
+        """
+        args = {
+            "file": file,
+            "line": line,
+            "offset": offset,
+            "prefix": prefix
+        }
+
+        response = self.send_request("completions", args, wait_for_response=True)
+
+        return self.__get_response_body(response)
+
+    def completion_entry_details(self, file, line, offset, entry_names):
+        """
+            Sends a "completionEntryDetails" request
+
+            :type file: string
+            :type line: int
+            :type offset: int
+            :type entry_names: array
+        """
+        args = {
+            "file": file,
+            "line": line,
+            "offset": offset,
+            "entryNames": entry_names
+        }
+
+        response = self.send_request("completionEntryDetails", args, wait_for_response=True)
+
+        return self.__get_response_body(response)
--- rplugin/python3/deoplete/sources/typescript.py
+++ rplugin/python3/deoplete/sources/typescript.py
@@ -1,13 +1,16 @@
 import os
 import re
-import json
-import subprocess
+import sys
 import platform
 import itertools
 
 from time import time
 from tempfile import NamedTemporaryFile
 from deoplete.source.base import Base
+
+sys.path.insert(1, os.path.dirname(__file__) + '/../..')
+
+from client import Client
 
 RELOAD_INTERVAL = 1
 MAX_COMPLETION_DETAIL = 25
@@ -19,97 +22,39 @@ class Source(Base):
     # Base options
     def __init__(self, vim):
         Base.__init__(self, vim)
+
         # Deoplete related
         self.debug_enabled = True
         self.name = "typescript"
         self.mark = "TS"
         self.filetypes = ["typescript", "tsx", "typescript.tsx", "javascript", "jsx", "javascript.jsx"] if vim.vars["deoplete#sources#tss#javascript_support"] else ["typescript"]
         self.rank = 700
-        self.input_pattern = r'\.\w*'
+        self.input_pattern = r"\.\w*"
         self._last_input_reload = time()
 
-        # Project related
-        self._project_directory = os.getcwd()
-        self._sequenceid = 0
-        self._environ = os.environ.copy()
-        self._tsserver_handle = None
+        # TSServer client
+        self._client = Client(debug_fn=self.debug, log_fn=self.log)
 
-    def startServer(self):
-        self.debug('startig')
-        self._tsserver_handle = subprocess.Popen("tsserver",
-                                                 env=self._environ,
-                                                 cwd=self._project_directory,
-                                                 stdout=subprocess.PIPE,
-                                                 stdin=subprocess.PIPE,
-                                                 stderr=subprocess.STDOUT,
-                                                 universal_newlines=True,
-                                                 shell=True,
-                                                 bufsize=1)
-        self.vim.command('echom "Deoplete-Typescript: Server Started"')
+    def log(self, message):
+        self.vim.command("echom '{}'".format(re.sub("'", "\'", message)))
 
-    def search_tss_project_dir(self, context):
-        self._project_directory = context['cwd']
-
-    def _sendReuest(self, command, arguments=None, wait_for_response=False):
-        seq = self._next_sequence_id()
-        request = {
-            "seq":     seq,
-            "type":    "request",
-            "command": command
-        }
-        if arguments:
-            request["arguments"] = arguments
-
-        self._write_message(request)
-        if not wait_for_response:
-            return
-
-        linecount = 0
-        headers = {}
-        while True:
-            headerline = self._tsserver_handle.stdout.readline().strip()
-            linecount += 1
-            if len(headerline):
-                key, value = headerline.split(":", 2)
-                headers[key.strip()] = value.strip()
-                if "Content-Length" not in headers:
-                    raise RuntimeError("Missing 'Content-Length' header")
-                contentlength = int(headers["Content-Length"])
-                ret = json.loads(self._tsserver_handle.stdout.read(contentlength))
-                if 'request_seq' not in ret or ret['request_seq'] > seq:
-                    return None
-                elif ret['request_seq'] == seq:
-                    return ret
-
-    def _write_message(self, message):
-        self._tsserver_handle.stdin.write(json.dumps(message))
-        self._tsserver_handle.stdin.write("\n")
-
-    def _reload(self):
+    def reload(self):
         filename = self.relative_file()
         contents = self.vim.eval("join(getline(1,'$'), \"\n\")")
+
         tmpfile = NamedTemporaryFile(delete=False)
-        tmpfile.write(contents.encode('utf-8'))
+        tmpfile.write(contents.encode("utf-8"))
         tmpfile.close()
-        self._sendReuest('reload', {
-            'file': filename,
-            'tmpfile': tmpfile.name
-        }, wait_for_response=True)
+
+        self._client.reload(filename, tmpfile.name)
+
         os.unlink(tmpfile.name)
 
     def relative_file(self):
         return self.vim.current.buffer.name
 
-    def _next_sequence_id(self):
-        seq = self._sequenceid
-        self._sequenceid += 1
-        return seq
-
     def on_event(self, context):
-        if self._tsserver_handle is None:
-            self.startServer()
-        if self._tsserver_handle is not None:
-            self._sendReuest('open', {'file': self.relative_file()})
+        self._client.open(self.relative_file())
 
     def get_complete_position(self, context):
         m = re.search(r"\w*$", context["input"])
@@ -119,43 +64,45 @@ class Source(Base):
         # reload if last reload expired or input completion is a method extraction
         if time() - self._last_input_reload > RELOAD_INTERVAL or re.search(r"\w*\.", context["input"]):
             self._last_input_reload = time()
-            self._reload()
-        data = self._sendReuest("completions", {
-            "file":   self.relative_file(),
-            "line":   context["position"][1],
-            "offset": context["complete_position"] + 1,
-            "prefix": context["complete_str"]
-        }, wait_for_response=True)
+            self.reload()
 
-        if data is None or not "body" in data:
+        data = self._client.completions(
+            file=self.relative_file(),
+            line=context["position"][1],
+            offset=context["complete_position"] + 1,
+            prefix=context["complete_str"]
+        )
+
+        if len(data) == 0:
             return []
 
-        if len(data["body"]) > MAX_COMPLETION_DETAIL:
+        if len(data) > MAX_COMPLETION_DETAIL:
             filtered = []
-            for entry in data["body"]:
+            for entry in data:
                 if (entry["kind"] != "warning"):
                     filtered.append(entry)
             return [self._convert_completion_data(e) for e in filtered]
 
         names = []
         maxNameLength = 0
-        for entry in data["body"]:
+
+        for entry in data:
             if (entry["kind"] != "warning"):
                 names.append(entry["name"])
                 maxNameLength = max(maxNameLength, len(entry["name"]))
 
-        detailed_data = self._sendReuest('completionEntryDetails', {
-            "file":   self.relative_file(),
-            "line":   context["position"][1],
-            "offset": context["complete_position"] + 1,
-            "entryNames": names
-        }, wait_for_response=True)
+        detailed_data = self._client.completion_entry_details(
+            file=self.relative_file(),
+            line=context["position"][1],
+            offset=context["complete_position"] + 1,
+            entry_names=names
+        )
 
-        if detailed_data is None or not "body" in detailed_data:
+        if len(detailed_data) == 0:
             return []
 
         return [self._convert_detailed_completion_data(e, padding=maxNameLength)
-                for e in detailed_data["body"]]
+                for e in detailed_data]
 
     def _convert_completion_data(self, entry):
         return {
@@ -164,20 +111,20 @@ class Source(Base):
         }
 
     def _convert_detailed_completion_data(self, entry, padding=80):
-        # self.debug(entry)
-
         name = entry["name"]
-        display_parts = entry['displayParts']
-        signature = ''.join([p['text'] for p in display_parts])
+        display_parts = entry["displayParts"]
+        signature = "".join([p["text"] for p in display_parts])
 
         # needed to strip new lines and indentation from the signature
-        signature = re.sub('\s+', ' ', signature)
-        menu_text = re.sub('^(var|let|const|class|\(method\)|\(property\)|enum|namespace|function|import|interface|type)\s+', '', signature)
+        signature = re.sub("\s+", " ", signature)
+        menu_text = re.sub("^(var|let|const|class|\(method\)|\(property\)|enum|namespace|function|import|interface|type)\s+", "", signature)
         documentation = menu_text
-        if 'documentation' in entry and entry['documentation']:
-            documentation += '\n' + ''.join([d['text'] for d in entry['documentation']])
+
+        if "documentation" in entry and entry["documentation"]:
+            documentation += "\n" + "".join([d["text"] for d in entry["documentation"]])
 
         kind = entry["kind"][0].title()
+
         return ({
             "word": name,
             "kind": kind,
